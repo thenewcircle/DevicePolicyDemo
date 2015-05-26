bdc2e1214580f56eea9650a32bb512a53ec459b3
Dave Smith <smith@wiresareobsolete.com>
2015-05-26 15:00:14 -0600

    Update file naming conventions.

--- app/src/main/java/com/example/android/devicepolicydemo/DevicePolicyDemoActivity.java
+++ app/src/main/java/com/example/android/devicepolicydemo/DevicePolicyDemoActivity.java
@@ -25,7 +25,7 @@ public class DevicePolicyDemoActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
 
         instructions = (TextView) findViewById(R.id.instructions);
         toggleButton = (Button) findViewById(R.id.toggle_device_admin);
